7e9a58c00293eebae7f15f8838c5e4e782b8b3e2
developerKamble <komaltanuja2714@gmail.com>
2022-04-10 22:34:07 +0530

    SpringBoot Projects

--- Core-Java1-8/Problem_statement3_1/Guitar.java
+++
@@ -1,9 +0,0 @@
-package com.coreproject1;
-
-public class Guitar extends Instrument {
-
-	public void play() {
-		System.out.println("Guitar is playing tin tin tin ");
-	}
-
-}
--- Core-Java1-8/Problem_statement3_2/Ointment.java
+++
@@ -1,8 +0,0 @@
-package com.coreproject2;
-
-public class Ointment implements MedicineInfo {
- 
-	public void displayLabel() {
-		System.out.println("for external use only");
-	}
-}
